6f57f1c7e6bf263d8b37f52d352c6490ed3dd0ad
praveena <pravina.parab@gmail.com>
2025-04-30 15:23:52 +0530

    commited hanges on 30 apr 25 extent

--- src/main/java/testBase/ExtentReportNG.java
+++ src/main/java/testBase/ExtentReportNG.java
@@ -19,7 +19,7 @@ public class ExtentReportNG {
 //        String reportPath = System.getProperty("user.dir")+
 //                "/Reports/ExecutionReport_"+actualDate+".html";
         String reportPath = System.getProperty("user.dir")+
-                "target/surefire-reports/Reports/ExecutionReport.html";
+                "Reports/ExecutionReport.html";
 
         ExtentSparkReporter sparkReport = new ExtentSparkReporter(reportPath);
 
